7f0e6596da6ad8049461f90a53958e5a689548b7
Prg-maker <df3296709@gmail.com>
2023-11-03 20:43:35 -0300

    add template project

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,61 @@
-
-import ReactFlow, { Background, Controls } from 'reactflow';
+import ReactFlow, {
+  Controls,
+  Background,
+  applyNodeChanges,
+  applyEdgeChanges,
+  addEdge,
+} from 'reactflow';
+import {useState, useCallback} from 'react'
 import 'reactflow/dist/style.css';
 
 
+let initialEdges = [{}]
+
+
+const initalNodes = [
+  {
+    id: '1',
+    position: { x: 0, y: 0 },
+    data:{label:"hello"},
+  },
+
+
+  {
+    id: '2',
+    position: { x: 100, y: 100 },
+    data:{label:"hello"},
+  },
+];
+
 function App() {
+
+  const [nodes , setNodes] = useState(initalNodes)
+  const [edges , setEdges] = useState(initialEdges) 
+
+  const onNodesChange= useCallback(
+    (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
+    []
+  )
+
+  const onEdgesChange = useCallback(
+    (changes) => setEdges((eds) => applyEdgeChanges(changes, eds)),
+    []
+  );
+  const onConnect = useCallback((params) => setEdges((eds) => addEdge(params, eds)), []);
+
 
   return (
     
-    <div style={{
-      width:'100vw',
-      height:'100vh',
-    }}>
-      <ReactFlow>
+    <div className="w-screen h-screen">
+      <ReactFlow 
+        nodes={nodes}
+        onNodesChange={onNodesChange}
+        edges={edges}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+      >
         <Background/>
+        <Controls />
       </ReactFlow>    
 
     </div>
---
+++ src/Components/Square.tsx
@@ -0,0 +1,7 @@
+export function Square(){
+  return (
+    <div>
+      
+    </div>
+  )
+}
