445a428c6bf7c697ce617a9b979d57d1c1f71e10
Prg-maker <df3296709@gmail.com>
2023-11-04 17:53:42 -0300

    add style

--- src/Components/Ellipse.tsx
+++ src/Components/Ellipse.tsx
@@ -6,7 +6,7 @@ export function Ellipse(props:NodeProps){
 
   return (
     <div className='bg-violet-500   rounded-[100%] w-[200px] h-[200px] flex items-center px-2 '>
-       <textarea  tabIndex={-1}  aria-hidden="true"  autoCorrect='off'  className=' h-[60px]  resize-none text-center leading bg-transparent py-4 text-white border-0 outline-none  placeholder:text-gray-300 placeholder-opacity-80 text-ellipsis overflow-hidden'  placeholder='Add text'/>
+        <textarea  autoCorrect='off'  className=' h-[60px] resize-none text-center leading bg-transparent py-4 text-white border-0 outline-none  placeholder:text-gray-300 placeholder-opacity-80 overflow-hidden nodrag'  placeholder='Add text'/>
     </div>    
 
   )
--- src/Components/Square.tsx
+++ src/Components/Square.tsx
@@ -1,18 +1,17 @@
+import { useCallback } from 'react'
 import {Handle, NodeProps, Position} from 'reactflow'
 
 export function Square(props:NodeProps){
-
+ 
   
+  //<textarea  onChange={onChange} autoCorrect='off'  className=' h-[60px] resize-none text-center leading bg-transparent py-4 text-white border-0 outline-none  placeholder:text-gray-300 placeholder-opacity-80 overflow-hidden '  placeholder='Add text'>
+  //</textarea>
 
   return (
     <div className='bg-violet-500   rounded w-[200px] h-[150px] flex items-center px-2'>
-      <Handle id="" type='source' position={Position.Left}/>
-
-       <textarea  tabIndex={-1}  aria-hidden="true"  autoCorrect='off'  className=' h-[60px] resize-none text-center leading bg-transparent py-4 text-white border-0 outline-none  placeholder:text-gray-300 placeholder-opacity-80 overflow-hidden '  placeholder='Add text'>
-         Aqui esta um bom exemplo
-       </textarea>
-
-      <Handle id="" type='source' position={Position.Right}/>
+      <Handle id="left" type='source' position={Position.Left}/>
+        <textarea  autoCorrect='off'  className=' h-[60px] resize-none text-center leading bg-transparent py-4 text-white border-0 outline-none  placeholder:text-gray-300 placeholder-opacity-80 overflow-hidden nodrag'  placeholder='Add text'/>
+      <Handle id="right" type='source' position={Position.Right}/>
 
     </div>    
 
