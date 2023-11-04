1b8f02fdd76666323b0f8dd73be7adc9cfac9744
Prg-maker <df3296709@gmail.com>
2023-11-04 14:41:18 -0300

    .

--- src/Components/Triangle.tsx
+++ src/Components/Triangle.tsx
@@ -7,10 +7,13 @@ export function Triangle(props:NodeProps){
   return (
    // <div className='bg-violet-500   rounded-[100%] w-[200px] h-[200px] flex items-center px-2 '>
    //    <textarea  tabIndex={-1}  aria-hidden="true"  autoCorrect='off'  className=' h-[60px]  resize-none text-center leading bg-transparent py-4 text-white border-0 outline-none  placeholder:text-gray-300 placeholder-opacity-80 text-ellipsis overflow-hidden'  placeholder='Add text'/>
-   // </div>    
+   // </div> 
+  
     <div className='triangle before:bg-violet-500 after:bg-violet-500'>
 
     </div>
+
+
 
   )
 }
